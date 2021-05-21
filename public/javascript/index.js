0e67c116e48cd626fde0d9b14472ffc0ded4e3cc
Debbie <debbiecamera0704@gmail.com>
2021-05-21 15:49:31 +0800

    modify UI

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,7 +3,7 @@ const copyText = document.getElementById('shortUrl');
 
 button.addEventListener('click', () => {
   copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  copyText.setSelectionRange(0, 99999);
 
   document.execCommand('copy');
   copyText.title = 'Copied!';
